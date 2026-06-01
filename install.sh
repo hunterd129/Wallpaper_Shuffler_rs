838fc0fbbbba9c05511e8e5e5e791fb5ca172360
hunterd129 <hunterd129@icloud.com>
2026-06-01 14:50:46 -0600

    added check for dms in niri.rs & dep check in install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
-# install.sh
+#check for dependencies
+if ! pkg-config --exists glib-2.0; then
+    echo -e "Missing dependencies needed to compile.\n PRESS ENTER to exit."
+    read -r
+    exit 1
+fi
 
-# Define paths
+if ! command -v rustc &>/dev/null; then
+    echo -e "missing dependencies needed to compile.\n ensure rust is installed.\n PRESS ENTER to exit."
+    read -r
+    exit 1
+fi
+
 SERVICE_DIR="$HOME/.config/systemd/user"
 APP_DIR="$HOME/.local/share/applications"
 BIN_DIR="$HOME/.local/bin"
 
-#Build binary
 echo "Building Wallpaper_Shuffler..."
 cargo build --release || {
     echo "Build failed. Check your Rust environment."
     exit 1
 }
-# Ensure dirs exist
 mkdir -p "$SERVICE_DIR" "$APP_DIR" "$BIN_DIR"
 
-#Move the binary
-cp "target/release/Wallpaper_Shuffler" "$BIN_DIR"
+mv "target/release/Wallpaper_Shuffler" "$BIN_DIR"
 
-# 1. Generate the service file
+# Generate wall_shuffd.service & .timer
 cat <<EOF >"$SERVICE_DIR/wall_shuffd.service"
 [Unit]
 Description=Trigger Wallpaper swap
@@ -46,7 +53,6 @@ Terminal=false
 Categories=Utility
 EOF
 
-#Generate the service timer
 cat <<EOF >"$SERVICE_DIR/wall_shuffd.timer"
 [Unit]
 Description=Schedule for wallpaper swap
@@ -58,6 +64,17 @@ Unit=wall_shuffd.service
 
 [Install]
 WantedBy=timers.target
+EOF
+
+# Generate .desktop entry
+cat <<EOF >"$APP_DIR/Wallpaper_Shuffler.desktop"
+[Desktop Entry]
+Name=Wallpaper_Shuffler
+Exec=$BIN_DIR/Wallpaper_Shuffler
+Icon=media-playlist-shuffle
+Type=Application
+Terminal=false
+Categories=Utility
 EOF
 
 # Reload and enable
--- src/os/linux/niri.rs
+++ src/os/linux/niri.rs
@@ -5,11 +5,22 @@ pub fn set_wallpaper(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let image_path = path.to_str().ok_or("Invalid path")?;
     let file_name = path.file_name().unwrap_or_default().to_string_lossy();
 
-    Command::new("dms")
-        .args(["ipc" , "wallpaper", "set", image_path])
-        .output()?;
+    let has_dms = Command::new("which")
+        .arg("dms")
+        .output()
+        .map(|o| o.status.success())
+        .unwrap_or(false);
 
-    println!("SUCCESS: Wallpaper successfully changed for Niri (DMS) Wayland Compositor");
-    println!("Image applied: {}", file_name);
+    if has_dms {
+        Command::new("dms")
+            .args(["ipc" , "wallpaper", "set", image_path])
+            .output()?;
+
+        println!("SUCCESS: Wallpaper successfully changed for Niri (DMS) Wayland Compositor");
+        println!("Image applied: {}", file_name);
+    } else {
+        println!("FAILURE: Currently, only dank material shell is supported.");
+    }
+    
     Ok(())
 }
