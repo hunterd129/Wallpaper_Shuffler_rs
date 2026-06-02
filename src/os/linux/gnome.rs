074db5e9b6c5d76216f650eb3a462b18e70fb45b
hunterd129 <hunterd129@icloud.com>
2026-06-02 17:21:13 -0600

    removed GIO crate. GNOME now uses Command::new. more consistent.

--- src/os/linux/gnome.rs
+++ src/os/linux/gnome.rs
@@ -1,17 +1,34 @@
 use std::path::Path;
-use gio::prelude::SettingsExt;
-use gio::Settings;
+use std::process::Command;
 
 pub fn set_wallpaper(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let image_path = path.to_str().ok_or("Invalid path")?;
     let file_name = path.file_name().unwrap_or_default().to_string_lossy();
 
-    let image_uri = format!("file://{}", image_path);
-    let settings = Settings::new("org.gnome.desktop.background");
-    settings.set_string("picture-uri", &image_uri)?;
-    settings.set_string("picture-uri-dark", &image_uri)?;
+    // GNOME expects the path to be formatted as a file:// URI schema
+    let uri = format!("file://{}", image_path);
 
-    println!("SUCCESS: Wallpaper successfully changed for GNOME desktop.");
+    // 1. Update the standard/light mode wallpaper schema
+    Command::new("gsettings")
+        .args([
+            "set",
+            "org.gnome.desktop.background",
+            "picture-uri",
+            &format!("'{}'", uri),
+        ])
+        .output()?;
+
+    // 2. Update the dark mode wallpaper schema 
+    Command::new("gsettings")
+        .args([
+            "set",
+            "org.gnome.desktop.background",
+            "picture-uri-dark",
+            &format!("'{}'", uri),
+        ])
+        .output()?;
+
+    println!("SUCCESS: Wallpaper successfully changed for GNOME Desktop via gsettings CLI.");
     println!("Image applied: {}", file_name);
     Ok(())
 }
