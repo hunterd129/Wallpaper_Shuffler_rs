bb54f3ece96e1d62c2cd42596287be01e468e38e
hunterd129 <hunterd129@icloud.com>
2026-06-02 17:43:01 -0600

    removed unneeded comments in gnome.rs

--- src/os/linux/gnome.rs
+++ src/os/linux/gnome.rs
@@ -5,10 +5,8 @@ pub fn set_wallpaper(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
     let image_path = path.to_str().ok_or("Invalid path")?;
     let file_name = path.file_name().unwrap_or_default().to_string_lossy();
 
-    // GNOME expects the path to be formatted as a file:// URI schema
     let uri = format!("file://{}", image_path);
 
-    // 1. Update the standard/light mode wallpaper schema
     Command::new("gsettings")
         .args([
             "set",
@@ -18,7 +16,6 @@ pub fn set_wallpaper(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
         ])
         .output()?;
 
-    // 2. Update the dark mode wallpaper schema 
     Command::new("gsettings")
         .args([
             "set",
